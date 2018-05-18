7f3ed2ac5ec9ba875cb7e1b07cfc2b5872f1b61c
Ho-Wan <ho.wan2345@gmail.com>
2018-05-18 11:25:49 +0100

    index.pug added.

--- Node_js_tutorial/Node+express/nodekb/app.js
+++ Node_js_tutorial/Node+express/nodekb/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const path = require('path');
 
+// Init app
 const app = express();
 
+// Load View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// Home Route
 app.get('/', function(req, res) {
-  res.send("Hello World");
+  res.render('index');
 });
 
 app.listen(3000, function() {
