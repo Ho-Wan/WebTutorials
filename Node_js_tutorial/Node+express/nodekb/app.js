c2908dfd1912810e0db6509aca86c19659dcc746
Ho-Wan <ho.wan2345@gmail.com>
2018-05-18 23:39:25 +0100

    app.js updated for mongoose. article.js added.

--- Node_js_tutorial/Node+express/nodekb/app.js
+++ Node_js_tutorial/Node+express/nodekb/app.js
@@ -1,48 +1,51 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodekb');
+let db = mongoose.connection;
+
+// Check connection
+db.once('open', function () {
+  console.log('Connected to MongoDB');
+});
+
+// check for DB errors
+db.on('error', function (err) {
+  console.log(err);
+});
 
 // Init app
 const app = express();
+
+// Bring in models
+let Article = require('./models/article');
 
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // Home Route
-app.get('/', function(req, res) {
-  let articles = [
-    {
-      id: 1,
-      title:'Article One',
-      author: 'Brad Traversy',
-      body: 'This is article one'
-    },
-    {
-      id: 2,
-      title:'Article Two',
-      author: 'John Doe',
-      body: 'This is article two'
-    },
-    {
-      id: 3,
-      title:'Article Three',
-      author: 'Brad Traversy',
-      body: 'This is article three'
-    },
-  ]
-  res.render('index', {
-    title: 'Articles',
-    articles: articles
+app.get('/', function (req, res) {
+  Article.find({}, function (err, articles) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', {
+        title: 'Articles',
+        articles: articles
+      });
+    }
   });
 });
 
 // Add Route
-app.get('/articles/add', function(req, res){
+app.get('/articles/add', function (req, res) {
   res.render('add_article', {
-    title:'Add Article'
+    title: 'Add Article'
   })
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000...");
 });
