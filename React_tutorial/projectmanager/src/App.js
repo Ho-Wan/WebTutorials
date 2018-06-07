fa005e16c718417cb365eda973ebc7ba43651df9
Ho-Wan <ho.wan2345@gmail.com>
2018-06-07 15:57:12 +0100

    project items output to view.

--- React_tutorial/projectmanager/src/App.js
+++ React_tutorial/projectmanager/src/App.js
@@ -3,11 +3,31 @@ import Projects from './Components/Projects';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      projects: [
+        {
+          title: 'Business website',
+          category: 'Web Design'
+        },
+        {
+          title: 'Social app',
+          category: 'Mobile Development'
+        },
+        {
+          title: 'E-commerce Shopping Cart',
+          category: 'Web Development'
+        },
+      ]
+      }
+    }
+
   render() {
     return (
       <div className="App">
         My App
-        <Projects test="Hello World"/>
+        <Projects projects={this.state.projects}/>
       </div>
     );
   }
